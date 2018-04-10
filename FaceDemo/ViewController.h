e27102c3ecc41c588dd89114f816794342330cea
liuwei <liuwei@wangfurobots.com>
2018-04-10 09:18:34 +0800

    Initial Commit

---
+++ FaceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceDemo
+//
+//  Created by liuwei on 2018/4/10.
+//  Copyright © 2018年 lw_local. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FaceDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FaceDemo
+//
+//  Created by liuwei on 2018/4/10.
+//  Copyright © 2018年 lw_local. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
